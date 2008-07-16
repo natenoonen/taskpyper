f35a8684d1c3d8e4e43c5f5935252bd9dc407515
FarMcKon <FarMcKon@87910592-7151-0410-9a4d-4915e747c23d>
2008-07-16 22:56:00 +0000

    misc updates, and started a taskRemover

--- grep.py
+++ grep.py
@@ -2,29 +2,27 @@ import re;
 
 
 class grep():
+	"""This is a qucik version of 'grep a list' I wrote for testing"""
 	def __init__(self,*args):
 		#print 'Foo'
 		str = None
 		target = None;
 		matches = [];
 		
-		#if(len(args) == 0):
-			#print 'empty foo'
+
+		if(len(args) == 0):
+			print 'No items! to search'
 		if(len(args) >= 1):
 			str = args[0];
 		if(len(args) >= 2):
 			target = args[1];
 		
+		#try to search for matches in the target	
 		if (str is not None) and  (target is not None):
 			reg = re.compile(re.escape(str),re.I)
-			#print 'reg'
-			#print reg
 			for item in target:
-				#print 'item'
-				#print item
 				if reg and (reg.search( item) is not None) :
-					matches.append( item );
-					#print item;
+					matches.append( item )
 		
 		if (matches is not None):
 			print matches
