57acabccb3cfdc829fc8f3bb795127a465d74c83
FarMcKon <FarMcKon@87910592-7151-0410-9a4d-4915e747c23d>
2008-07-29 22:23:48 +0000

    minor tweaks

--- taskpyperTest.py
+++ taskpyperTest.py
@@ -2,13 +2,17 @@ import unittest;
 import taskpyper;
 
 
-__version__  = u"0.1.0.10"
-gDebug = True;
+#These are semi-standard magic values to track module info
+__license__ = u"LGPL"
+__version__  = u"0.1.0.16";
+__author__ = u"FarMcKon";
+
+_gDebug = True;
 
 #:todo: hide this function from the outside world
 def debugprint(*stuffToPrint):
 	#Astric indicates that it can handle a list as input
-	if (gDebug):
+	if (_gDebug):
 		print "DEBUG:" + " ".join(stuffToPrint)
 		
 		
